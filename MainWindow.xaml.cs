5c5de8e831358fab10f957c8b08b7b05285ef804
eymenefealtun <eymenefealtun@hotmail.com>
2024-02-09 13:43:40 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TypingSpeedApplication
+{
+    public partial class MainWindow : Window
+    {
+        private string _lightTheme = "#f0ece4";
+        private string _darkTheme = "#1e1e1e";
+        BrushConverter _brushConverter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _brushConverter = new BrushConverter();
+
+            Label label1 = new Label();
+            label1.Content = "My Words";
+
+            Label label2 = new Label();
+            label2.Content = "My Second Words";
+
+            stckPanel1.Children.Clear();
+
+            stckPanel1.Children.Add(label1);
+            stckPanel1.Children.Add(label2);
+        }
+
+        private void btnBackgroungTheme_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = btnBackgroungTheme.IsChecked == true ? (SolidColorBrush)_brushConverter.ConvertFromString(_lightTheme) : (SolidColorBrush)_brushConverter.ConvertFromString(_darkTheme);
+        }
+
+
+    }
+}
