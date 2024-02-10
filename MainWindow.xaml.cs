63fec74a39e279c12b9a64fc643f48a9ff17e53e
eymenefealtun <eymenefealtun@hotmail.com>
2024-02-10 18:29:07 +0300

    feat: timer added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace TypingSpeedApplication
 {
@@ -9,22 +11,47 @@ namespace TypingSpeedApplication
         private string _lightTheme = "#f0ece4";
         private string _darkTheme = "#1e1e1e";
         BrushConverter _brushConverter;
+        DispatcherTimer _dispatcherTimer;
+
+        int _second = 7;
+        public int Second
+        {
+            get
+            {
+                return this._second;
+            }
+            set
+            {
+                _second = value;
+                lblTimer.Content = _second.ToString();
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
             _brushConverter = new BrushConverter();
 
-            Label label1 = new Label();
-            label1.Content = "My Words";
 
-            Label label2 = new Label();
-            label2.Content = "My Second Words";
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
 
-            stckPanel1.Children.Clear();
 
-            stckPanel1.Children.Add(label1);
-            stckPanel1.Children.Add(label2);
+
+            _dispatcherTimer.Start();
+
+        }
+
+        private void dispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            Second--;
+
+            if (Second == 0)
+            {
+                _dispatcherTimer.Stop();
+                lblTimer.Content = "60";
+            }
         }
 
         private void btnBackgroungTheme_Click(object sender, RoutedEventArgs e)
